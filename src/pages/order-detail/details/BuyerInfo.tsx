911cfb8c0d3eb0672564a554a99c8ac093e8c235
Jirayu Yung <50855666+Jirayu321@users.noreply.github.com>
2024-10-17 17:19:06 +0700

    update 17 oct 05

--- src/pages/order-detail/details/BuyerInfo.tsx
+++ src/pages/order-detail/details/BuyerInfo.tsx
@@ -49,7 +49,7 @@ const BuyerInfo: React.FC<{ buyer: any }> = ({ buyer }) => {
         </Typography>
         <Typography variant="body1">
           <strong>LINE ID:</strong>{" "}
-          {buyer.Line_id ? `${buyer.Cust_line}` : ``}
+          {buyer.Cust_line ? `${buyer.Cust_line}` : ``}
         </Typography>
       </Box>
     </Stack>
